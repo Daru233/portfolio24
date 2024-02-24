f069bfde7be369d49f1516eae7271e1ad78db7ec
Michael Malto <michaeldm233@gmail.com>
2024-02-24 21:42:52 +0000

    initial bento box

--- portfolio24/src/App.js
+++ portfolio24/src/App.js
@@ -1,11 +1,7 @@
-import Career from "./sections/work/Career.js";
-import Splash from "./sections/splash/Splash.js";
-import NavBar from "./NavBar/NavBar.js";
 import { useRef } from "react";
-import "./App.css";
-import EmailFixed from "./fixed/email/EmailFixed.js";
+import NavBar from "./NavBar/NavBar.js";
 import IconsFied from "./fixed/Icons/IconsFixed.js";
-import Projects from "./sections/projects/Projects.js";
+import "./App.css";
 
 function App() {
     const splashRef = useRef();
@@ -19,11 +15,43 @@ function App() {
                 splashRef={splashRef}
                 projectsRef={projectsRef}
             />
-            <EmailFixed />
             <IconsFied />
-            <Splash splashRef={splashRef} />
-            <Career careerRef={careerRef} />
-            <Projects projectsRef={projectsRef} />
+            <main className="grid-container">
+                <div className="splash-grid-container">
+                    <div className="left-side">
+                        <p>Hi, I'm</p>
+                        <h1>DARIUS</h1>
+                        <h3>Always aiming to improve</h3>
+                    </div>
+                    <div className="right-side">
+                        <h2>DESIGN</h2>
+                        <h2>DEVELOP</h2>
+                        <h2>DEPLOY</h2>
+                        <h2>SOLUTIONS</h2>
+                    </div>
+                </div>
+
+                <div className="box">
+                    <h3>Github</h3>
+                    <p>Hi, I'm</p>
+                </div>
+                <div className="box profile">
+                    <h3>About Me</h3>
+                    <p>Hi, I'm</p>
+                </div>
+                <div className="box">
+                    <h3>LinkedIn</h3>
+                    <p>Hi, I'm</p>
+                </div>
+                <div className="box">
+                    <h3>title</h3>
+                    <p>Hi, I'm</p>
+                </div>
+                <div className="box">
+                    <h3>title</h3>
+                    <p>Hi, I'm</p>
+                </div>
+            </main>
         </div>
     );
 }
